8ff3452763c2c50c23d4a1c84484740a51a9ee42
juanmacozzuol <juanmacozzuol@gmail.com>
2023-11-23 12:00:42 -0300

    added handlebars

---
+++ routes/views.router.js
@@ -0,0 +1,20 @@
+import {Router} from 'express'
+import ProductManager from '../classes/ProductManager.js'
+
+let manager = new ProductManager('productos.json')
+let productos = manager.getProducts()
+
+const router = Router()
+
+router.get('/',(req,res)=>{
+ 
+    res.render('home',{productos})
+})
+
+router.get('/realtimeproducts',(req,res)=>{
+ 
+    res.render('home',{productos})
+})
+
+
+export default router
--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import express from 'express'
 import productRouter from '../routes/product.router.js'
 import cartRouter from '../routes/cart.router.js'
-
+import viewsRouter from '../routes/views.router.js'
+import handlebars from 'express-handlebars'
+import __dirname from '../utils.js'
 
 const app = express()
 const port = 8080
 
+app.engine('handlebars', handlebars.engine())
+app.set('views',__dirname+'/views')
+app.set('view engine','handlebars')
+app.use(express.static(__dirname+'/public'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/api/products',productRouter)
 app.use('/api/carts',cartRouter)
+app.use('/',viewsRouter)
 
 
 
